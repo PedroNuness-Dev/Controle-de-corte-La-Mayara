d119fbee3216bfe18af7d1a7ce16ce17132e3f56
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-08-15 12:17:51 -0300

    fix: corrige ausencia de alguns atributos que continham na classe mas não no .sql

--- Controle-de-Corte-La-Mayara/src/main/resources/db/migration/V3__create_tables_lista_e_item.sql
+++ Controle-de-Corte-La-Mayara/src/main/resources/db/migration/V3__create_tables_lista_e_item.sql
@@ -2,17 +2,19 @@ CREATE TABLE lista(
                       id INTEGER PRIMARY KEY AUTO_INCREMENT,
                       titulo VARCHAR(100) NOT NULL,
                       descricao VARCHAR(250),
-                      dataDeCriacao DATE NOT NULL DEFAULT current_date
+                      data_de_criacao DATE NOT NULL DEFAULT current_date
 );
 
 
 CREATE TABLE item(
                      id INTEGER PRIMARY KEY AUTO_INCREMENT,
                      nome VARCHAR(100) NOT NULL,
+                     quantidade INTEGER NOT NULL,
                      observacao VARCHAR(250),
                      atencao BOOLEAN NOT NULL DEFAULT FALSE,
                      posicao INTEGER NOT NULL,
-                     lista INTEGER,
+                     lista_id INTEGER,
+                     data_de_insercao DATE NOT NULL DEFAULT current_date,
 
-                     CONSTRAINT fk_item_lista FOREIGN KEY (lista) REFERENCES lista(id)
+                     CONSTRAINT fk_item_lista FOREIGN KEY (lista_id) REFERENCES lista(id)
 );
