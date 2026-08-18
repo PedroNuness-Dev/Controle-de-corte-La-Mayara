d0bfa9b321bde7d4c582e4fb78778884dfd52f60
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-08-18 20:51:17 -0300

    feat: ativa o cache no projeto com a anotação @EnableCaching

--- Controle-de-Corte-La-Mayara/src/main/java/com/PedroNunesDev/Controle_de_Corte/ControleDeCorteApplication.java
+++ Controle-de-Corte-La-Mayara/src/main/java/com/PedroNunesDev/Controle_de_Corte/ControleDeCorteApplication.java
@@ -2,8 +2,10 @@ package com.PedroNunesDev.Controle_de_Corte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ControleDeCorteApplication {
 
 	public static void main(String[] args) {
