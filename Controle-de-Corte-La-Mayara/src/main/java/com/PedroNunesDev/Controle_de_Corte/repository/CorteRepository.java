9bed226a4b22f21be642b9b8a40e6a6a259eb739
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-08-20 00:10:11 -0300

    feat: cria nova consulta para buscar os cortes entre um intervalo de datas e pelo status

--- Controle-de-Corte-La-Mayara/src/main/java/com/PedroNunesDev/Controle_de_Corte/repository/CorteRepository.java
+++ Controle-de-Corte-La-Mayara/src/main/java/com/PedroNunesDev/Controle_de_Corte/repository/CorteRepository.java
@@ -1,5 +1,6 @@
 package com.PedroNunesDev.Controle_de_Corte.repository;
 
+import com.PedroNunesDev.Controle_de_Corte.enums.CorteStatus;
 import com.PedroNunesDev.Controle_de_Corte.model.Corte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +15,12 @@ public interface CorteRepository extends JpaRepository<Corte,Long> {
             " WHERE c.dataDeRegistro BETWEEN :diaPrimeiro AND :diaUltimo" +
             " ORDER BY c.nomeModelo ASC")
     List<Corte> buscarPorMes(@Param("diaPrimeiro") LocalDate diaPrimeiro,@Param("diaUltimo") LocalDate diaUltimo);
+
+    @Query("SELECT c FROM Corte c" +
+            " WHERE c.dataDeRegistro BETWEEN :diaPrimeiro AND :diaUltimo" +
+            " AND c.corteStatus = :status" +
+            " ORDER BY c.nomeModelo ASC")
+    List<Corte> buscarPorMesEPorStatus(@Param("diaPrimeiro") LocalDate diaPrimeiro, @Param("diaUltimo") LocalDate diaUltimo, @Param("status") CorteStatus status);
 
     @Query("SELECT c FROM Corte c" +
             " WHERE c.dataDeRegistro BETWEEN :diaPrimeiro AND :diaUltimo" +
